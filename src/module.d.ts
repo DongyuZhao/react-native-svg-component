77a8bf1d9a16e550d89a8a2e8bde8280092fc660
Dongyu Zhao <donz@microsoft.com>
2018-12-03 19:42:45 +0800

    Fix the type definition

--- src/module.d.ts
+++ src/module.d.ts
@@ -1,18 +1,16 @@
-declare module 'Svg' {
-    import React from 'react';
-    import { ImageSourcePropType, StyleProp, ViewStyle } from 'react-native';
+import React from 'react';
+import { ImageSourcePropType, StyleProp, ViewStyle } from 'react-native';
 
-    interface IProps {
-        source: ImageSourcePropType;
-        alt: string;
-        width?: number;
-        height?: number;
-        style?: StyleProp<ViewStyle>;
-    }
-
-    interface IState {
-        data: string;
-    }
-  
-    export const Svg: React.ComponentClass<IProps, IState>;
+interface IProps {
+    source: ImageSourcePropType;
+    alt: string;
+    width?: number;
+    height?: number;
+    style?: StyleProp<ViewStyle>;
 }
+
+interface IState {
+    data: string;
+}
+
+export const Svg: React.ComponentClass<IProps, IState>;
